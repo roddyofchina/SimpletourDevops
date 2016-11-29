9e3805b94c29be5431e32377047060c449091b48
roddyofchina <923401910@qq.com>
2016-11-29 12:48:20 +0800

    更改dwebsocket为eventlet

---
+++ run_eventlet.py
@@ -0,0 +1,16 @@
+from eventlet import wsgi, patcher
+patcher.monkey_patch()
+
+import sys
+import getopt
+import eventlet
+from SimpletourDevops.wsgi import application
+
+
+addr, port = '127.0.0.1', 8099
+opts, _ = getopt.getopt(sys.argv[1:], "b:")
+for opt, value in opts:
+    if opt == '-b':
+        addr, port = value.split(":")
+
+wsgi.server(eventlet.listen((addr, int(port))), application)
